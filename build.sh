1c8bec4ee71e5a22c935dba5669b596685edc478
w2599 <114452622+w2599@users.noreply.github.com>
2025-12-31 21:58:03 +0800

    Xcode 26.2

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# 进入脚本所在目录
+cd "$(dirname -- "$0")"
+export THEOS=/Users/zqbb/theos_roothide
+# export DEVELOPER_DIR="/Applications/Xcode-14.3.0.app/Contents/Developer"
+
 PREV_DIR=$(pwd)
 WORK_DIR=$(dirname -- "$0")
 cd "$WORK_DIR"
